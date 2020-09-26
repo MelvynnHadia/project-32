f9947e0d8da7211df06ee92bac6d37d4b5793612
MelvynnHadia <69381490+MelvynnHadia@users.noreply.github.com>
2020-09-26 20:25:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,114 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+var stand1;
+var ball;
+var slingShot;
+var score = 0;        
+var polygon_img;
+function preload(){
+  polygon_img=loadImage("polygon.png");
+}
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground();
+  stand1 = new Stand(390,300,250,10);
+
+  //level two
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  //level three
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  //top
+  block16 = new Block(390,155,30,40);
+
+  //ball holder with slings
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  slingShot = new Slingshot(this.ball,{x:100,y:200});
+
+}
+function draw() {
+  background(56,44,44); 
+  text(mouseX + ',' + mouseY, 10, 15);
+  textSize(20);
+  fill("lightyellow");
+  text("Drag the polygon to destroy the blocks",300,30);
+  text("SCORE : "+score,750,40);
+  textSize(10);
+  text("Press Space to get a second Chance to Play!!",650 ,350);
+  keyPressed();
+
+  ground.display();
+  stand1.display();
+  strokeWeight(2);
+  stroke(15);
+  fill("pink");
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  fill("turquoise");
+  block13.display();
+  block14.display();
+  block15.display();
+  fill("grey");
+  block16.display();
+
+  fill("gold");
+  imageMode(CENTER)
+  image(polygon_img ,ball.position.x,ball.position.y,40,40);
+
+  slingShot.display();
+
+  block8.score();
+  block9.score();
+  block10.score();
+  block11.score();
+  block12.score();
+  block13.score();
+  block14.score();
+  block15.score();
+  block16.score();
+
+}
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+  slingShot.fly();
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        slingShot.attach(this.ball);
+    }
+}
+
+async function getBackground(){
+  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = await response.json();
+
+  var datetime = responseJSON.datetime;
+  var hour = datetime.slice(11, 13);
+
+  if (hour >= 06 && hour <= 18) {
+    background("white")
+  } else {
+    background("brown");
+  }
+}
